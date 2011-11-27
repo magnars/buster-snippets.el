407deca3c8ebd4ddce62f1f0e0555ac37a6d7206
Magnar Sveen <magnars@gmail.com>
2011-11-27 11:00:14 +0100

    add buster-snippets to yasnippet load directory

--- buster-snippets.el
+++ buster-snippets.el
@@ -1,2 +1,10 @@
 (require 'snippet-helpers)
+
+;; Find buster-snippets root directory
+(setq buster-snippets-root (file-name-directory
+                            (or (buffer-file-name) load-file-name)))
+
+;; Load snippets
+(yas/load-directory (expand-file-name "snippets" dotfiles-dir))
+
 (provide 'buster-snippets)
